7f40e10bd449185c4f30583ba09e3cf3f7b985e0
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2022-11-24 00:23:37 -0600

    add secure cookies in collections fix 5

--- payload.config.js
+++ payload.config.js
@@ -3,7 +3,8 @@ import Representatives from './collections/Representatives';
 import Users from './collections/Users';
 import PageContent from './collections/PageContent';
 //import {seed} from './seed';
-import path from 'path'
+import path from 'path';
+import dotenv from 'dotenv';
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
