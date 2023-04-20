736cadb467254e8bea6ca16362401340cfc82445
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2023-04-19 21:23:04 -0500

    fix xlsx endpoint

--- customComponents/CustomList.js
+++ customComponents/CustomList.js
@@ -10,7 +10,7 @@ const CustomList = (props) => {
   const Click = async (e) => {
     e.preventDefault();
     window.open(
-      `http://app.overton.services/dashboardServices/xls-process?clientId=${clientId}`
+      `https://app.overton.services/dashboardServices/xls-process?clientId=${clientId}`
     );
   };
   return (
