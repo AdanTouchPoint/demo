28f8b1aa4f1cd00859936cb75df523219428263c
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2023-05-24 14:50:00 -0500

    kentavrifidata service migrated

---
+++ controllers/kenttaVerify.js
@@ -0,0 +1,28 @@
+const axios = require('axios')
+let phonePass
+let emailPass
+
+const verifyPhone = async (phone) => {
+  console.log(phone);
+  phonePass = await axios.post(
+    `http://apilayer.net/api/validate?access_key=${process.env.PHONE_KEY_KENTTA}&number=${phone}&country_code=MX&format=1`
+  );
+  return phonePass.data.valid;
+};
+const verifyEmail = async (email) => {
+  console.log(email);
+  emailPass = await axios.post(
+    `https://api.emailable.com/v1/verify?email=${email}&api_key=${process.env.EMAIL_KEY_KENTTA}`
+  );
+  return emailPass.data.score;
+};
+const verify = async (phone, email) => {
+  if (phone) {
+    let valid = await verifyPhone(phone).catch((e) => console.error(e));
+    let score = await verifyEmail(email).catch((e) => console.error(e));
+    return { valid, score };
+  }
+  let score = await verifyEmail(email).catch((e) => console.error(e));
+  return { score };
+};
+module.exports = verify ;
--- routes/dashboardServices.js
+++ routes/dashboardServices.js
@@ -1,131 +1,178 @@
-const {Router} = require('express');
+const { Router } = require("express");
 const router = Router();
-const processExcel = require("../controllers/convertToXls")
+const processExcel = require("../controllers/convertToXls");
 const sendEmail = require("../controllers/emailController");
 const keywords = require("../controllers/restrictedWords.js");
-const checker = require('../controllers/wordsCheck');
+const checker = require("../controllers/wordsCheck");
+const verify = require("../controllers/kenttaVerify");
 
 router.get("/xls-process", async (req, res) => {
-    try {
-      const { clientId } = req.query;
-      const buffer  =  await processExcel.processExcel(clientId)
-      // Verificar que el tamaño del archivo no exceda el límite permitido (en este ejemplo, 5 MB)
-      const maxSize = 5 * 1024 * 1024; // 5 MB en bytes
-      if (buffer.byteLength > maxSize) {
-        throw new Error("El archivo generado es demasiado grande");
-      }
-        res.contentType('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        res.status(200)
-        res.attachment('leads.xlsx')
-        res.send(Buffer.from(buffer));
-    } catch (error) {
-      res.status(400);
-      res.json({
-        success: false,
-        message: error.message,
-      });
+  try {
+    const { clientId } = req.query;
+    const buffer = await processExcel.processExcel(clientId);
+    // Verificar que el tamaño del archivo no exceda el límite permitido (en este ejemplo, 5 MB)
+    const maxSize = 5 * 1024 * 1024; // 5 MB en bytes
+    if (buffer.byteLength > maxSize) {
+      throw new Error("El archivo generado es demasiado grande");
     }
-  });
-  router.get("/send-email", async (req, res) => {
-    try {
-      const query = req.query;
-      console.log(query);
-      const email = await sendEmail.contact_email(query);
-      res.json({
-        success: true,
-        message: "Email Sent",
-        data: email,
-      });
-    } catch (error) {
-      res.status(400);
-      res.json({
-        success: false,
-        message: error.message,
-      });
-    }
-  }); 
-  router.get("/email-builder", async (req, res) => {
+    res.contentType(
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.status(200);
+    res.attachment("leads.xlsx");
+    res.send(Buffer.from(buffer));
+  } catch (error) {
+    res.status(400);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+router.get("/send-email", async (req, res) => {
+  try {
     const query = req.query;
-    try {
-     let email 
-     let user = JSON.parse(query.user)
-     let questions = JSON.parse(query.questions);
-     const checkText = await checker(questions, keywords)
-     if(checkText)
-     {
-      email = await sendEmail.emailBuilder(questions,user);
-     }
-      res.json({
-        success: true,
-        data: email,
-      });
-    } catch (error) {
-      res.status(400);
-      res.json({
-        success: false,
-        message: error.message,
-      });
+    console.log(query);
+    const email = await sendEmail.contact_email(query);
+    res.json({
+      success: true,
+      message: "Email Sent",
+      data: email,
+    });
+  } catch (error) {
+    res.status(400);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+router.get("/email-builder", async (req, res) => {
+  const query = req.query;
+  try {
+    let email;
+    let user = JSON.parse(query.user);
+    let questions = JSON.parse(query.questions);
+    const checkText = await checker(questions, keywords);
+    if (checkText) {
+      email = await sendEmail.emailBuilder(questions, user);
     }
-  });
-  router.get("/email-batch", async (req, res) => {
-    try {
-      const query = req.query;
-      console.log(query);
-      const email = await sendEmail.batch_email(query);
-      res.json({
-        success: true,
-        message: "Email Sent",
-        data: email,
-      });
-    } catch (error) {
-      res.status(400);
-      res.json({
-        success: false,
-        message: error.message,
-      });
-    }
-  });
-  router.get("/email-state", async (req, res) => {
-    try {
-      const query = req.query;
-      const content = await payload.find({
-        collection: "diputados-y-senadores",
-        sort: "-updatedAt",
-        where: {
-          clientId: {
-            equals: query.clientId,
-          },
-          and: [
-            {
-              state: {
-                equals: query.state,
-              },
-            },
-          ],
+    res.json({
+      success: true,
+      data: email,
+    });
+  } catch (error) {
+    res.status(400);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+router.get("/email-batch", async (req, res) => {
+  try {
+    const query = req.query;
+    console.log(query);
+    const email = await sendEmail.batch_email(query);
+    res.json({
+      success: true,
+      message: "Email Sent",
+      data: email,
+    });
+  } catch (error) {
+    res.status(400);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+router.get("/email-state", async (req, res) => {
+  try {
+    const query = req.query;
+    const content = await payload.find({
+      collection: "diputados-y-senadores",
+      sort: "-updatedAt",
+      where: {
+        clientId: {
+          equals: query.clientId,
         },
-      });
-      let data = content.docs;
-      let states = await data.map((el) => {
-        return el.contact;
-      });
-      let contact = [...new Set(states)];
-      let strings = contact.toString();
-      query.to = strings;
-      console.log(query);
-      const email = await sendEmail.batch_email(query);
-      //console.log(query)
-      res.json({
-        success: true,
-        message: "Email Sent",
-        data: email,
-      });
-    } catch (error) {
-      res.status(400);
-      res.json({
-        success: false,
-        message: error.message,
-      });
-    }
-  });
+        and: [
+          {
+            state: {
+              equals: query.state,
+            },
+          },
+        ],
+      },
+    });
+    let data = content.docs;
+    let states = await data.map((el) => {
+      return el.contact;
+    });
+    let contact = [...new Set(states)];
+    let strings = contact.toString();
+    query.to = strings;
+    console.log(query);
+    const email = await sendEmail.batch_email(query);
+    //console.log(query)
+    res.json({
+      success: true,
+      message: "Email Sent",
+      data: email,
+    });
+  } catch (error) {
+    res.status(400);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
-  module.exports = router
+router.get("/kentta-data-validator", async (req, res) => {
+  try {
+    const { email, phone } = req.query;
+    verify(phone, email)
+      .then(async (data) => {
+        if (data.valid === false) {
+          res.status(208);
+          res.json({
+            success: false,
+            message: "Telefono invalido , introduzca otro numero",
+          });
+          return;
+        }
+        if (data.score >= 50) {
+          console.log(`${data.score} Muy Bueno`);
+          res.json({
+            success: true,
+            message: "Correo Valido",
+          });
+        } else if (data.score >= 25) {
+          console.log(`${data.score}  Regular`);
+          res.status(206);
+          res.json({
+            success: false,
+            message: "intenta con otro  correo",
+          });
+        } else if (data.score >= 0) {
+          console.log(`${data.score}  Mal`);
+          res.status(206);
+          res.json({
+            success: false,
+            message: "intenta con otro  correo",
+          });
+        }
+      })
+      .catch((e) => console.error(e));
+  } catch (error) {
+    res.status(400);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+module.exports = router;
